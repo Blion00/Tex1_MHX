3f5b1d213bd875b84202fdb0ce33c28770ff08e8
Gia Bảo <92194463+Blion00@users.noreply.github.com>
2024-07-21 22:52:48 +0700

    updata

--- Buoi4/B4.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByTagName('button');
-// setAttribute: thêm và cập nhật giá trị 
-const buttonNode = document.getElementById("buttonid");
-buttonNode.onclick=function(){
-    alert("hello");
-}
-// getItem(key) : lấy giá trị từ localStogal
